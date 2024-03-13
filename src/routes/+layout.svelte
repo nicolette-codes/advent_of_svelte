7c32df3320aef6c6407515dc14a2ef35aa609e9b
Nicolette | Nikki <info@nicolette.codes>
2024-03-13 16:19:55 +0100

    project init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Advent of Svelte</title>
+</svelte:head>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="">Advent of svelte (5)</h1>
+<button class="btn btn-primary">Click me</button>
